b3b13b810131fe23400569db1f8d7d918e7ac3cb
machiobrian <machiobrian29@gmail.com>
2024-04-12 19:13:43 +0300

    See - Carl Burch’s expla- nation in “About the Towers of Hanoi,” http://mng.bz/c1i2.

---
+++ smallProblems/towersOfHanoi/hanoi.py
@@ -0,0 +1,38 @@
+# model a stack in python using a list as a backing store
+
+from typing import TypeVar, Generic, List
+T = TypeVar('T') # define an arbitrary type T
+
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        self._container: List[T] = []
+    def pushDisc(self, item:T) -> None:
+        self._container.append(item)
+    def popDisc(self) -> T:
+        return self._container.pop()
+    def __repr__(self) -> str:
+        return repr(self._container)    
+    
+# define our towers as Stacks and fill them with discs
+num_discs: int = 3
+tower_a: Stack[int] = Stack()
+tower_b: Stack[int] = Stack()
+tower_c: Stack[int] = Stack()
+
+for i in range(1, num_discs+1):
+    tower_a.pushDisc(i)
+
+
+def hanoi(begin: Stack[int], end: Stack[int], temp: Stack[int], n: int) -> None:
+    if n == 1:
+        end.pushDisc(begin.popDisc())
+    else:
+        hanoi(begin, temp, end, n-1)
+        hanoi(begin, end, temp, 1)
+        hanoi(temp, end, begin, n-1)
+
+if __name__ == "__main__":
+    hanoi(tower_a, tower_c, tower_b, num_discs)
+    print(tower_a)
+    print(tower_b)
+    print(tower_c)
